b95317fae5ce2ce8ab7fe6a7932c6eacc23faf14
JulianaOnofrio <julianaonofrio@hotmail.com>
2021-06-11 15:25:52 -0300

    First commit

---
+++ src/main/java/br/com/juliana/questionforum/QuestionForumApplication.java
@@ -0,0 +1,13 @@
+package br.com.juliana.questionforum;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QuestionForumApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QuestionForumApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/juliana/questionforum/QuestionForumApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.juliana.questionforum;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QuestionForumApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
